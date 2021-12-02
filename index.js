e39e13917e0149b8636f47a0c10684c34b9382f5
André Carello <andrelino.cl@gmail.com>
2021-12-02 17:34:45 -0300

    fix

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-import Vue from "vue";
-import SChart from "./src/components/s-chart";
-
-const Components = {
-    SChart
-}
-
-Object.keys(Components).forEach(name => Vue.component(name, Components[name]))
-
-export default Components;
+export { default as SChart } from './src/components/s-chart.vue';
